456317971e99cfd2ce255cb3161ecfb5281240cc
zoe <Zoe@onehumble.com>
2024-08-27 14:59:41 +0800

    Updates

--- preLoad/js/index.js
+++ preLoad/js/index.js
@@ -1,16 +1,23 @@
 console.log("isPreLoad!!!!")
+console.log($,"prevObject")
+
 function preload(){
     let preload = document.createElement("div")
-    preload.setAttribute("id","preload")
-    document.getElementsByTagName("header")[0].after(preload)
-    const xhr = new XMLHttpRequest();
+    // preload.setAttribute("style","background:pink;z-index:9999!important;position:absolute;top:0;height:100vh;width:100vw")
+    // console.log(preload,"preload")
+    console.log(HTMLCollection.namedItem("body"),"document.getElementsByTagName(\"body\")")
+     document.getElementsByTagName("body")[0].append(preload)
+    // preload.setAttribute("id","preload")
+    // document.getElementsByTagName("body")[0].before(preload)
+    // const xhr = new XMLHttpRequest();
     let thePath = window.location.href.replace(window.location.pathname,"/pages/preLoad/index.html")
     xhr.open("GET", thePath, true);
     xhr.send();
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 4 && xhr.status == 200) {
             preload.innerHTML = xhr.responseText
-            ispreload=true
+            // ispreload=true
         }
     }
 }
+preload()
